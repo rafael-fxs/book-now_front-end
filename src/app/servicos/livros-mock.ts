f61df88514bcf6bd37d5e00de62c1ebe35bd3d88
Rafael Felipe <rafa_el_felipe@hotmail.com>
2024-08-11 23:03:55 -0300

    Melhorado mock

--- src/app/servicos/livros-mock.ts
+++ src/app/servicos/livros-mock.ts
@@ -1,14 +1,14 @@
 export const LIVROS_MOCK = [
-    { id: 1, nome: 'Livro 1', categoria: 'Categoria A', descricao: 'teste', pontos: 10 },
-    { id: 2, nome: 'Livro 2', categoria: 'Categoria B', descricao: 'teste', pontos: 20 },
-    { id: 3, nome: 'Livro 3', categoria: 'Categoria C', descricao: 'teste', pontos: 30 },
-    { id: 4, nome: 'Livro 4', categoria: 'Categoria D', descricao: 'teste', pontos: 40 },
-    { id: 5, nome: 'Livro 5', categoria: 'Categoria E', descricao: 'teste', pontos: 50 },
-    { id: 6, nome: 'Livro 6', categoria: 'Categoria F', descricao: 'teste', pontos: 60 },
-    { id: 7, nome: 'Livro 7', categoria: 'Categoria G', descricao: 'teste', pontos: 70 },
-    { id: 8, nome: 'Livro 8', categoria: 'Categoria H', descricao: 'teste', pontos: 80 },
-    { id: 9, nome: 'Livro 9', categoria: 'Categoria I', descricao: 'teste', pontos: 90 },
-    { id: 10, nome: 'Livro 10', categoria: 'Categoria J', descricao: 'teste', pontos: 100 },
-    { id: 11, nome: 'Livro 11', categoria: 'Categoria K', descricao: 'teste', pontos: 110 }
+    { id: 1, nome: 'Livro 1', categoria: 'A', descricao: 'teste', pontos: 10 },
+    { id: 2, nome: 'Livro 2', categoria: 'B', descricao: 'teste', pontos: 20 },
+    { id: 3, nome: 'Livro 3', categoria: 'C', descricao: 'teste', pontos: 30 },
+    { id: 4, nome: 'Livro 4', categoria: 'D', descricao: 'teste', pontos: 40 },
+    { id: 5, nome: 'Livro 5', categoria: 'E', descricao: 'teste', pontos: 50 },
+    { id: 6, nome: 'Livro 6', categoria: 'F', descricao: 'teste', pontos: 60 },
+    { id: 7, nome: 'Livro 7', categoria: 'G', descricao: 'teste', pontos: 70 },
+    { id: 8, nome: 'Livro 8', categoria: 'H', descricao: 'teste', pontos: 80 },
+    { id: 9, nome: 'Livro 9', categoria: 'I', descricao: 'teste', pontos: 90 },
+    { id: 10, nome: 'Livro 10', categoria: 'J', descricao: 'teste', pontos: 100 },
+    { id: 11, nome: 'Livro 11', categoria: 'K', descricao: 'teste', pontos: 110 }
   ];
   
